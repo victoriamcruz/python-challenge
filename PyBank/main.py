530753a0f586349f7ae0d6b7e685b2323f2ecc79
Victoria Cruz <victoriacruz32@gmail.com>
2020-07-24 21:37:17 -0500

    new excel

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,23 @@
-#this is for python HW
+#python bank 
+
+import os
+
+
+current_working_directory = os.getcwd()
+print(current_working_directory)
+
+
+
+# csvpath = os.path.join( "..", "..", "Desktop", "SchoolFile", "HWRepos", "python-challenge", "Pybank", "Resources", "PyBank.csv")
+
+# with open(Pybank.csv) as csvfile:
+
+#     # CSV reader specifies delimiter and variable that holds contents
+#     csvreader = csv.reader(csvfile, delimiter=',')
+
+#     print(csvreader)
+
+
+# list.count = ['January', 'Feburary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+# month_count = list.count('January', 'Feburary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')
+# print(' the total number of months, month_count')
