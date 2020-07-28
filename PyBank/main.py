8284f9cd66d2784ffc6d6634caa6093d654c157d
Victoria Cruz <victoriacruz32@gmail.com>
2020-07-28 01:05:11 -0500

    homework update

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,23 +1,82 @@
-#python bank 
+#pybank
 
 import os
 
+# current_working_directory = os.getcwd()
+# print(current_working_directory)
 
-current_working_directory = os.getcwd()
-print(current_working_directory)
+# Module for reading CSV files
+import csv
+
+Total_Count = 0
+Total_Months = []
+
+Net = 0
+Total_Net = []
+
+Monthly_Change = []
+Date = []
+
+Greatest_Increase = 0
+Greatest_Increase_Month = 0
+Greatest_Decrease = 0
+Greatest_Decrease_Month = 0
 
 
+#define file path to open
+csvpath = os.path.join("..", "PyBank", "Resource", "PyBank.csv")
 
-# csvpath = os.path.join( "..", "..", "Desktop", "SchoolFile", "HWRepos", "python-challenge", "Pybank", "Resources", "PyBank.csv")
+#CSV reader specifies delimiter and variable that holds contents
+with open(csvpath) as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter = ',')
+    csv_header = next(csv_reader)
+    # print(f"CSV Header: {csv_header}")
+    row = next(csv_reader)
+    # print(row)
+    
+    previous_row = int(row[1])
+    Total_Count += 1
+    Net += int(row[1])
 
-# with open(Pybank.csv) as csvfile:
+    for row in csv_reader:
+        Total_Count += 1
+        Net += int(row[1])
+        Date.append(row[0])
+        Net_Change = int(row[1]) - previous_row
+        Monthly_Change.append(Net_Change)  
+        previous_row = int(row[1])
+        Total_Months.append(row[0])
 
-#     # CSV reader specifies delimiter and variable that holds contents
-#     csvreader = csv.reader(csvfile, delimiter=',')
+        
+        if int(row[1]) > Greatest_Increase: 
+            Greatest_Increase = int(row[1])
+            Greatest_Increase_Month = row[0]
 
-#     print(csvreader)
+        if int(row[1]) < Greatest_Decrease:
+            Greatest_Decrease = int(row[1])
+            Greatest_Decrease_Month = row[0]
+         
+        Max = max(Monthly_Change)
+        Min = min(Monthly_Change)
 
+        Average = round(sum(Monthly_Change)/ len(Monthly_Change), 2)
 
-# list.count = ['January', 'Feburary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-# month_count = list.count('January', 'Feburary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')
-# print(' the total number of months, month_count')
+print("Financial Analysis")
+print("----------------------------------------------------------")
+print(f"Total_Months: {Total_Count}")
+print(f"Total_Net: ${Net}")
+print(f"Average_Change: {Average}") 
+print(f"Greatest_Increase: {Greatest_Increase_Month}, ${Max}")
+print(f"Greatest_Decrease: {Greatest_Decrease_Month}, ${Min}")
+
+output_file = os.path.join("..", "PyBank", "Analysis", "PyBank.text")
+
+with open(output_file, 'w') as txtfile:
+
+    txtfile.write("Financial Analysis\n")
+    txtfile.write("----------------------------------------------------------\n")
+    txtfile.write(f"Total_Months: {Total_Count}\n")
+    txtfile.write(f"Total_Net: ${Net}\n")
+    txtfile.write(f"Average_Change: {Average}\n") 
+    txtfile.write(f"Greatest_Increase: {Greatest_Increase_Month}, (${Max})\n")
+    txtfile.write(f"Greatest_Decrease: {Greatest_Decrease_Month}, (${Min})\n")
--- PyPoll/mainpoll.py
+++ PyPoll/mainpoll.py
@@ -1,1 +1,66 @@
 #this will be used for poll hw
+
+import os
+import csv
+
+vote_count = 0
+candidates_list = []
+total_votes = []
+candidate_votes = {}
+
+csvpath = os.path.join( "..", "PyPoll", "Resource", "Poll Data.csv")
+
+  
+with open(csvpath) as csvfile:
+  electionreader = csv.reader(csvfile, delimiter = ',')
+  csv_header = next(electionreader)
+  # row = next(electionreader)
+
+  for row in electionreader:
+    vote_count += 1
+    candidates = (row[2])
+    if candidates not in candidates_list:
+        candidates_list.append(candidates)
+        candidate_votes[candidates] = 0
+    candidate_votes[candidates] = candidate_votes[candidates] + 1
+    # Total_Net += int(row[1])
+
+print("Election Results")
+print("------------------")
+print(f"Total Votes: {vote_count}")
+# print(candidates_list)
+# print(candidate_votes)
+potential_candidate = candidates_list[0]
+winning_votes = candidate_votes[potential_candidate]
+
+print("------------------")
+
+for key in candidate_votes:
+    print(f" {key}, {round(100 * candidate_votes[key]/vote_count, 2)}%,  ({candidate_votes[key]})")
+    if candidate_votes[key] > winning_votes:
+        potential_candidate = key
+        winning_votes = candidate_votes[key]
+print("------------------")
+print(f"Winner: {potential_candidate} with {winning_votes} votes!")    
+
+print("------------------")
+
+# Specify File To Write To
+output_file = os.path.join("..", "PyPoll", "Analysis", "Poll_Data_remix.text")
+
+# Open File Using "Write" Mode. Specify The Variable To Hold The Contents
+with open(output_file, 'w') as txtfile:
+
+# Write New Data
+    txtfile.write("Election Results\n")
+    txtfile.write("---------------------------\n")
+    txtfile.write(f"Total Votes: {vote_count}\n")
+    txtfile.write("---------------------------\n")
+    for key in candidate_votes:
+      txtfile.write(f"{key}, {round(100 * candidate_votes[key]/vote_count, 2)}%,  ({candidate_votes[key]})\n")
+  
+    txtfile.write("--------------------------\n")
+
+    txtfile.write(f"Winner: {potential_candidate} with {winning_votes} votes!\n")  
+    
+    txtfile.write("---------------------------\n")
